e9cab7ab4949ff7ce1a6a09ccb78ab2dcc1645aa
tjmax26 <teejayogidan@gmail.com>
2026-05-18 23:28:07 +0100

    funtionalities were added

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const sub = (a,b)=> a - b
 
 const mul = (a,b) => a * b
 
-
 const div = (a,b) => a / b
 
 let num1;
 let operator;
 let num2;
+let current = "0";
 
 const operate = (operator,num1,num2) =>{
     if(operator === "+") return add(num1,num2);
@@ -18,7 +18,7 @@ const operate = (operator,num1,num2) =>{
     if(operator ==="/") return div(num1,num2) 
 
 }
-// console.log(operate("/",2,2));
+// console.log(operate("+",2,2));
 
  const container = document.querySelector("#container")
 
@@ -32,8 +32,10 @@ container.appendChild(clearButton);
 
 
 clearButton.addEventListener("click",() =>{
+    current = "0"
     display.textContent = "0"
 })
+
 
 
 
@@ -41,9 +43,28 @@ const buttons = document.querySelectorAll("button")
 buttons.forEach((button) =>{
     button.addEventListener("click",()=>{
 
-    if(button.textContent !="+" && button.textContent !=="-" && button.textContent !=="*" && button.textContent !=="/" && button.textContent !=="=" && button.textContent !=="CLR"){
-        display.textContent += button.textContent
-    }
-    })
+    if( button.textContent !=="+" && button.textContent !=="-" && button.textContent !=="*" && button.textContent !=="/" && button.textContent !=="=" && button.textContent !=="CLR"){
+        if(current ==="0" && button.textContent !== "."){
+            current = button.textContent
+        }else{
+            current += button.textContent
+        }
+        display.textContent = current
 
+}
+   
+if(button.textContent ==="+" || button.textContent === "-" || button.textContent === "*" || button.textContent === "/"){
+  num1 = +current;
+operator = button.textContent;
+current = "0"
+}
+
+if (button.textContent === "="){
+    num2 = parseFloat(current);
+    let result = operate(operator,num1,num2);
+    display.textContent = result;
+}
+    })
+ 
 })
+
