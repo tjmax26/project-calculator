08c41052f7e58a582b678b16ae532957de5a3486
tjmax26 <teejayogidan@gmail.com>
2026-05-20 02:26:59 +0100

    features added to make the calculator function well

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const sub = (a,b)=> a - b
 
 const mul = (a,b) => a * b
 
-const div = (a,b) => a / b
 
+const div = (a,b)=>{
+    if (b===0) return "Error!"
+    return a/b
+}
+ 
 let num1;
 let operator;
 let num2;
@@ -20,23 +24,21 @@ const operate = (operator,num1,num2) =>{
 }
 // console.log(operate("+",2,2));
 
+const display = document.querySelector("#display");
+display.textContent = "0";
+
  const container = document.querySelector("#container")
-
- const display = document.querySelector("#display");
- display.textContent = "0";
-
 
 const clearButton = document.createElement("button");
 clearButton.textContent = "CLR";
 container.appendChild(clearButton);
 
-
 clearButton.addEventListener("click",() =>{
     current = "0"
+    num1 = undefined;
+    operator = undefined;
     display.textContent = "0"
-})
-
-
+}) 
 
 
 const buttons = document.querySelectorAll("button")
@@ -44,33 +46,57 @@ buttons.forEach((button) =>{
     button.addEventListener("click",()=>{
 
     if( button.textContent !=="+" && button.textContent !=="-" && button.textContent !=="*" && button.textContent !=="/" && button.textContent !=="=" && button.textContent !=="CLR"){
-        if(current ==="0" && button.textContent !== "."){
-            current = button.textContent
+        if(current ==="0"){
+            current = button.textContent;
         }else{
-            current += button.textContent
+            current += button.textContent;
         }
-        display.textContent = current
 
+        display.textContent = current;
 }
+
+ // --- if it's an OPERATOR button ---
    
 if(button.textContent ==="+" || button.textContent === "-" || button.textContent === "*" || button.textContent === "/"){
-  num1 = +current;
-operator = button.textContent;
+ 
+if(num1 !==undefined){
+    let result = operate(operator,num1,parseFloat(current));
+    if(result === "Error!"){
+          display.textContent = result;
+        num1 = undefined;
+        operator = undefined;
+        current = "0";
+        return;
+    }
+    display.textContent = +(result.toFixed(4));
+    num1 = result
+}else {
+    num1 = +current
+}
+operator = button.textContent
 current = "0"
+
+
 }
 
+    // --- if it's the EQUALS button ---
 if (button.textContent === "="){
     if(num1 === undefined || operator === undefined) return;
     num2 = parseFloat(current);
     let result = operate(operator,num1,num2);
-    display.textContent = +(result.toFixed(4));
-    current = "0"
+    if(result === "Error!"){
+        display.textContent = result
+    }else{
+
+        display.textContent = +(result.toFixed(4));
+    }
+    current = "0";
     num1 = undefined;
-    operator = undefined
+    operator = undefined;
     
     
 }
     })
  
-})
+});
 
